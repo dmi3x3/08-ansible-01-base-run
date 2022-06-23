910b479b48a91b0243c0b6c9e1280e07ddfb2bb6
Dmitriy Kapustin <dima.kap@mail.ru>
2022-06-23 06:46:47 +0300

    add tag not surely part

--- start_stop.sh
+++ start_stop.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 docker run --name centos7 -d pycontribs/centos:7 /bin/bash -c "while :; do echo 'Press <CTRL+C> to exit.'; sleep 1; done"
 docker run --name ubuntu -d pycontribs/ubuntu /bin/bash -c "while :; do echo 'Press <CTRL+C> to exit.'; sleep 1; done"
+docker run --name fedora_ -d pycontribs/fedora /bin/bash -c "while :; do echo 'Press <CTRL+C> to exit.'; sleep 1; done"
 
+echo "netology" > ans-vault.key
+ansible-playbook -i inventory/prod.yml site.yml --vault-password-file ans-vault.key
+rm ans-vault.key
 
-#docker stop centos7
-#docker stop ubuntu
+docker stop ubuntu centos7 fedora_ && docker rm ubuntu centos7 fedora_
